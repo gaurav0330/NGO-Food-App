7c7f090845ed8d1c8c543ce3f98f1d61fe8f6b4a
gaurav0330 <gauravjikar070806@gmail.com>
2024-03-01 16:25:51 +0530

    screen created

--- lib/model/user/event_org.dart
+++ lib/model/user/event_org.dart
@@ -1,10 +1,10 @@
 class EventOrg {
-  String _name;
-  String _location;
-  String _phone;
-  String _email;
-  var _donation;
+  final String name;
+  final String location;
+  final String phone;
+  final String email;
+  final List donation;
+  EventOrg({required this.name, required this.location, required this.phone, required this.email, required this.donation});
 
-  EventOrg(this._name, this._location, this._phone, this._email, this._donation);
 }
 
